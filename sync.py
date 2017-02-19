8acd9d1cb05afa60733188d44bb6257d0b9a443f
abunuwas <joseharoperalta@gmail.com>
2017-02-19 18:03:48 +0000

    Catch exceptions and unify data format

--- sync.py
+++ sync.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sys
 from xml.etree import ElementTree as ET
+import zipfile
 from zipfile import ZipFile
 
 import boto3
@@ -9,22 +10,39 @@ import boto3
 s3 = boto3.client('s3')
 
 
+def trim_tpl(tpl):
+    try:
+        key, value = tpl
+        return key.lstrip(), value.lstrip()
+    except (IndexError, ValueError):
+        return (None, None)
+
+
 def to_dict(metadata_list):
-    tuples = (line.split(':') for line in metadata_list)
-    clean = map(lambda tpl: (tpl[0].lstrip(), tpl[1].lstrip()), tuples)
+    tuples = tuple(line.split(':', 1) for line in metadata_list)
+    clean = tuple(map(trim_tpl, tuples))
     return dict(clean)
 
 
 def pdf_metadata(file):
     metadata = subprocess.check_output(['pdfinfo', '-meta', file]).decode('utf-8')
+    if 'Metadata' in metadata:
+        metadata = metadata[:metadata.find('Metadata')]
     metadata_list = metadata.splitlines()
-    return to_dict(metadata_list)
+    metadata_dict = to_dict(metadata_list)
+    return {
+        'Author': metadata_dict.get('Author'),
+        'Title': metadata_dict.get('Title'),
+        'Publisher': metadata_dict.get('Pulbisher'),
+        'Date': metadata_dict.get('ModDate')
+    }
 
 
 def get_text_from_xml(xml, search_for):
     for el in xml:
         if search_for in el.tag:
             return el.text
+    return None
 
 
 def extract_metadata(metadata_xml):
@@ -55,18 +73,24 @@ def get_files(dir):
 
 def yield_data(dir):
     for file in get_files(dir):
-        print('Parsing file {}...'.format(file))
-        if file.endswith('.pdf'):
-            yield pdf_metadata(file)
-        elif file.endswith('.epub'):
-            yield epub_metadata(file)
-        else:
+        try:
+            print('Parsing file {}...'.format(file))
+            if file.endswith('.pdf'):
+                yield pdf_metadata(file)
+            elif file.endswith('.epub'):
+                yield epub_metadata(file)
+            else:
+                yield {}
+        except zipfile.BadZipFile:
             yield {}
 
 
 if __name__ == '__main__':
     dir = sys.argv[1]
     print('Parsing directory {}...'.format(dir))
+    e=0
     for data in yield_data(dir):
         for d in data.items():
             print(d)
+        e+=1
+    print(e)
