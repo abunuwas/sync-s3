7669adfcc529156108658d7595b43b02676ac2f2
abunuwas <joseharoperalta@gmail.com>
2017-02-20 22:55:37 +0000

    Clean code and add sort function key

--- cleaup.py
+++ cleaup.py
@@ -1,6 +1,7 @@
 from difflib import SequenceMatcher
 import Levenshtein
 import os
+import re
 
 
 def similar(one, two):
@@ -8,10 +9,14 @@ def similar(one, two):
     return Levenshtein.ratio(one, two)
 
 
+def sorting(value):
+    return re.sub('_|\s|\d|\(|\)', 'z', value).lower()
+
+
 def get_files(file):
-    with open('new_files.txt') as f:
+    with open(file) as f:
         files = f.read()
-    return files.splitlines()
+    return sorted(set(files.splitlines()), key=sorting)
 
 
 def get_similars(files):
@@ -22,30 +27,38 @@ def get_similars(files):
     return outcome
 
 
-if __name__ == '__main__':
-    files = get_files('new_files.txt')
-    files = sorted(files, key=str.lower)
+def remove_duplicates(files):
     written = []
-    with open('new_files_clean.txt', 'w') as f:
-        for file in files:
-            if len(written) > 0:
-                similars = [w for w in written if .8 < similar(file, w) < .99]
-                if len(similars) > 0:
-                    decision = input('\n\nThe following file {} has the following'
-                                     ' similar files already written to the file:\n'
-                                     '{}.\nShould it be written? (y/n)'.format(
-                        file, ','.join(similars))
-                    )
-                    if decision == 'y':
-                        written.append(file)
-                elif len([w for w in written if similar(file, w) == 1]) >= 1:
-                    pass
-                else:
+    for file in files:
+        if len(written) > 0:
+            similars = [w for w in written if .8 < similar(file, w) < .99]
+            if len(similars) > 0:
+                decision = input('\n\nThe following file {} has the following'
+                                 ' similar files already written to the file:\n'
+                                 '{}.\nShould it be written? (y/n)'.format(
+                    file, ','.join(similars))
+                )
+                if decision == 'y':
                     written.append(file)
+            elif len([w for w in written if similar(file, w) == 1]) >= 1:
+                pass
             else:
                 written.append(file)
+        else:
+            written.append(file)
+    return written
 
+
+def to_file(files, file):
     print('#'*30)
-    for w in written:
-        print(w)
-    print(len(written))
+    with open(file, 'w') as f:
+        for w in files:
+            f.write(w + '\n')
+            print(w)
+    print(len(files))
+
+
+if __name__ == '__main__':
+    files = get_files('new_files.txt')
+    good_files = remove_duplicates(files)
+    to_file(good_files, 'new_files_clean.txt')
